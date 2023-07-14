6bbe17090bf80412dd97261bfcde9b7efff23bc5
SeldomseenSchweig <jamesschweig@gmail.com>
2023-07-14 12:05:43 -0700

    logged a back end route

--- routes/auth.js
+++ routes/auth.js
@@ -61,6 +61,7 @@ router.post("/register", async function (req, res, next) {
     }
 
     const newUser = await User.register({ ...req.body, isAdmin: false });
+    console.log("NEW USER     ******************",newUser);
     const token = createToken(newUser);
     return res.status(201).json({ token });
   } catch (err) {
